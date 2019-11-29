9a2604e5d1796a2b533bc8f3c3a03ed82bdf8a37
Elizabeth Cruz <e.cruzramirez@ugto.mx>
2019-11-28 18:41:57 -0600

    Mejora de la vista de registro, validaciones de la vista registro. Armado del catalogo

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,11 +2,40 @@
 const express=require('express'); 
 const router= express.Router();
 
+const User=require('../models/User')
+
 // <----------------- RUTA PARA EL REGISTRO DE USUARIOS ------------------>
 router.get('/users/singin',(req,res)=>{
     res.render('users/singin.hbs');
 });
-
+router.post('/users/singin', async(req,res)=>{
+    const {Nombre, Apellido, Email, select, password, password_conf}= req.body;
+    const errors = [];
+    if(Nombre.length <= 0){
+        errors.push({text:'Inserte su nombre'});
+    }
+    if(password != password_conf){
+        errors.push({text: 'No coincide el password'});
+    }
+    if(password.length < 4){
+        errors.push({text: 'El password debe ser mayor a 4 caracteres'});
+    }
+    if(errors.length > 0){
+        res.render('users/singin', {errors, Nombre, Apellido, Email, select, password, password_conf});
+    } else{
+       const emailuser=  await User.findOne({Email: Email});
+       if(emailuser){
+           req.flash('error_msg', 'El email ya esta en uso');
+           res.redirect('/users/singin');
+       }
+       const newUser = new User({Nombre, Apellido, Email, select, password});
+       newUser.password= await newUser.encryptPassword(password);
+       await newUser.save(); 
+       req.flash('ESTAS REGISTRADO');
+       res.redirect('/users/login');
+    }
+});
+ 
 // <----------------- RUTA PARA EL ACCESO DE USUARIOS   ------------------>
 router.get('/users/login',(req,res)=>{
     res.render('users/login.hbs');
